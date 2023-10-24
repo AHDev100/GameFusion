4fd05bfcfc89ca548d3c79161d9eace1cb0742c8
Arun Hariharan <arunhariharan21@gmail.com>
2023-10-24 15:55:14 -0400

    Added frontend + backend dependencies

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello There");
+const app = require('express')(); 
+
+const port = 8000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
