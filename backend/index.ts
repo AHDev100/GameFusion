92f499caf6cce26393a93d5c52f4bc04a90e7957
Arun Hariharan <arunhariharan21@gmail.com>
2023-10-24 22:30:53 -0400

    Typescript Backend Config is Getting There

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,12 @@
-const app = require('express')(); 
+import express, { Express, Request, Response } from 'express';
 
+const app : Express = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
