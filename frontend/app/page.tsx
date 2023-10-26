4aea977eb2d2603b54c025176bd43e777a3d9ca9
Arun Hariharan <arunhariharan21@gmail.com>
2023-10-25 21:28:34 -0400

    Just tinkering with TSX and Tailwind, starting Langing Page tomorrow

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { useState } from "react"
+import Button from "./components/button";
 
 export default function Home() {
   const [count, setCount] = useState(0); 
   return (
-    <>
-      <p>Current count is {count}</p>
-      <button onClick={() => setCount(count + 1)}>Click Here</button>
-    </>
+    <div className="h-screen flex items-center justify-center">
+      <Button onClick = 
+        {() : void => {
+          alert(`Setting count to ${count + 1}`);
+          setCount(count + 1);
+        }} 
+      />
+      <p className="mr-4">Current count is {count}</p>
+    </div>
   )
 }
