00cc708da7ddfc46609a36ca947fc57ac58d5451
AHDev100 <107712922+AHDev100@users.noreply.github.com>
2023-10-26 22:36:08 -0400

    Completed Navbar, added all base routes using Next Routing  (#2)
    * Just tinkering with TSX and Tailwind, starting Langing Page tomorrow
    
    * Completed Navbar, added all base routes using Next Routing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,6 @@
-"use client";
-
-import { useState } from "react"
-
-export default function Home() {
-  const [count, setCount] = useState(0); 
+export default function HomePage() { 
   return (
-    <>
-      <p>Current count is {count}</p>
-      <button onClick={() => setCount(count + 1)}>Click Here</button>
-    </>
+    <div>
+    </div>
   )
 }
