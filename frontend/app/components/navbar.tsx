90894216b344ac413a49d1a7489df92792935f25
Arun Hariharan <arunhariharan21@gmail.com>
2023-10-26 22:47:20 -0400

    Deleted uncessary route - brain fart

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -10,7 +10,7 @@ export const Navbar = () : any => {
                 </div>
                 <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                     <div className="text-sm lg:flex-grow flex justify-center">
-                        <a href="/home" className="block mt-4 lg:inline-block lg:mt-0 text-neutral-200 hover:text-white hover:underline mr-4 text-base">
+                        <a href="/" className="block mt-4 lg:inline-block lg:mt-0 text-neutral-200 hover:text-white hover:underline mr-4 text-base">
                             Home
                         </a>
                         <a href="/community" className="block mt-4 lg:inline-block lg:mt-0 text-neutral-200 hover:text-white hover:underline mr-4 text-base">
