a9462d76d96e94175519aa03611ee7c56cfd1394
Arun Hariharan <arunhariharan21@gmail.com>
2023-11-10 18:25:21 -0500

    Setup Redis and Sequelize ORM - Now need to work on sessions for login and register routes

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,13 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import typeDefs from './graphql/gameSchema.js';
 import resolvers from './graphql/gameResolvers.js';
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+import { createClient } from 'redis';
+import connectRedis from 'connect-redis';
+import session from 'express-session';
+
+dotenv.config();
 
 const app = express();
 
@@ -9,6 +16,27 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+const startRedis = async () => {
+  const redisClient = createClient(); 
+  await redisClient.connect();
+  await redisClient.set('mykey', 'Hello from node redis');
+  const myKeyValue = await redisClient.get('mykey');
+  console.log(myKeyValue);
+}
+
+const db = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PWD}`, {
+  host: 'localhost', 
+  dialect: 'postgres', 
+  port: 5432,
+});
+
+db.authenticate().then(() => {
+  console.log('Connection to GameFusion db established successfully!');
+  startRedis();
+}).catch((err) => {
+  console.error('Unable to connect: ', err)
+})
 
 async function startServer() {
   await server.start();
