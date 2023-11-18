593e6780e2d4606a0fcb34684f84a69aff1d6b89
Arun Hariharan <arunhariharan21@gmail.com>
2023-11-17 23:03:08 -0500

    Navbar rendering issue addressed with temporary load, added registration mutation - routing not working though after promise resolves

--- frontend/app/utils/login.tsx
+++ frontend/app/utils/login.tsx
@@ -1,9 +1,10 @@
+"use client"
+
 import { useEffect, useState } from 'react';
 import { useRouter, usePathname } from "next/navigation";
 
 function LoggedOut(){
     const router = useRouter();
-    const pathname = usePathname();
 
     function navigateloginPage(){
         router.push("/auth/login");
@@ -36,26 +37,39 @@ function LoggedIn() {
 }
 
 export default function Status() {
-  const [token, setToken] = useState<any>(null);
-  const router = useRouter();
-  const pathname = usePathname();
-
-  useEffect(() => {
-    function handleStorageChange() {
-      setToken(sessionStorage.getItem('token'));
-    }
-
-    handleStorageChange();
-  }, []);
-
-  useEffect(() => {
-    // token ? router.push('/dashboard') : router.push('/');
-    if(token && (pathname == '/dashboard' || pathname == '/auth/login')){
-        router.push('/dashboard'); 
-    } else if (!token && (pathname == '/'|| pathname == '/auth/logout')){
-        router.push('/');
-    }
-  }, [token]);
-
-  return token ? <LoggedIn /> : <LoggedOut />;
-}
+    const [token, setToken] = useState<any>(null);
+    const [loading, setLoading] = useState(true); // Add a new state for loading
+    const router = useRouter();
+    const pathname = usePathname();
+  
+    useEffect(() => {
+      function handleStorageChange() {
+        setToken(sessionStorage.getItem('token'));
+        setLoading(false); // Set loading to false after token is updated
+      }
+  
+      handleStorageChange();
+    }, []);
+  
+    useEffect(() => {
+      if (!loading) { // Only push a new route if not loading
+        if (token && (pathname == '/dashboard' || pathname == '/auth/login')) {
+          router.push('/dashboard'); 
+        } else if (!token && (pathname == '/'|| pathname == '/auth/logout')) {
+          router.push('/');
+        }
+      }
+    }, [token, loading]);
+  
+    // Render a placeholder button if loading, otherwise render based on token
+    return loading ? <PlaceholderButton /> : (token ? <LoggedIn /> : <LoggedOut />);
+  }
+  
+  function PlaceholderButton() {
+    return (
+      <div>
+        <button className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-red-800 hover:bg-white mt-4 lg:mt-0 mx-2" disabled>Loading</button>
+      </div>
+    );
+  }
+  
